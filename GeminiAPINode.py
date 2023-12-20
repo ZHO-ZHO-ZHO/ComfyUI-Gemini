38f9cc6d8f8ebb0f3d7c047504043bd3eeb198d3
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2023-12-20 16:29:36 +0800

    Update GeminiAPINode.py

--- GeminiAPINode.py
+++ GeminiAPINode.py
@@ -4,7 +4,7 @@ import torch
 import google.generativeai as genai
 from io import BytesIO
 from PIL import Image
-
+from Gemini_API_Key import get_gemini_api_key 
 
 class Gemini_API_Zho:
 
@@ -130,6 +130,123 @@ class Gemini_API_Vsion_ImgURL_Zho:
         return (textoutput,)
 
 
+class Gemini_API_S_Zho:
+
+    def __init__(self):
+        self.api_key = get_gemini_api_key()  # 从Gemini_API_Key.py获取API密钥
+        if self.api_key is not None:
+            genai.configure(api_key=self.api_key)
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "prompt": ("STRING", {"default": "What is the meaning of life?", "multiline": True}),
+                "model_name": (["gemini-pro", "gemini-pro-vision"],),
+                "stream": ("BOOLEAN", {"default": False}),
+            },
+            "optional": {
+                "image": ("IMAGE",),  
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("text",)
+    FUNCTION = "generate_content"
+
+    CATEGORY = "Zho模块组/✨Gemini"
+
+    def tensor_to_image(self, tensor):
+        # 确保张量是在CPU上
+        tensor = tensor.cpu()
+    
+        # 将张量数据转换为0-255范围并转换为整数
+        # 这里假设张量已经是H x W x C格式
+        image_np = tensor.squeeze().mul(255).clamp(0, 255).byte().numpy()
+    
+        # 创建PIL图像
+        image = Image.fromarray(image_np, mode='RGB')
+        return image
+
+    def generate_content(self, prompt, model_name, stream, image=None):
+        if not self.api_key:
+            raise ValueError("API key is required")
+
+        model = genai.GenerativeModel(model_name)
+
+        if model_name == 'gemini-pro':
+            if stream:
+                response = model.generate_content(prompt, stream=True)
+                textoutput = "\n".join([chunk.text for chunk in response])
+            else:
+                response = model.generate_content(prompt)
+                textoutput = response.text
+        
+        if model_name == 'gemini-pro-vision':
+            if image == None:
+                raise ValueError("gemini-pro-vision needs image")
+            else:
+                # 转换图像
+                pil_image = self.tensor_to_image(image)
+
+                # 直接使用PIL图像
+                if stream:
+                    response = model.generate_content([prompt, pil_image], stream=True)
+                    textoutput = "\n".join([chunk.text for chunk in response])
+                else:
+                    response = model.generate_content([prompt, pil_image])
+                    textoutput = response.text
+        
+        return (textoutput,)
+
+
+class Gemini_API_S_Vsion_ImgURL_Zho:
+
+    def __init__(self):
+        self.api_key = get_gemini_api_key()  # 从Gemini_API_Key.py获取API密钥
+        if self.api_key is not None:
+            genai.configure(api_key=self.api_key)
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "prompt": ("STRING", {"default": "Describe this image", "multiline": True}),
+                "image_url": ("STRING", {"default": ""}),
+                "model_name": (["gemini-pro-vision"],),
+                "stream": ("BOOLEAN", {"default": False}),
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("text",)
+    FUNCTION = "generate_content"
+
+    CATEGORY = "Zho模块组/✨Gemini"
+
+    def generate_content(self, prompt, model_name, stream, image_url):
+        if not self.api_key:
+            raise ValueError("API key is required")
+
+        # Load the image from the URL
+        response = requests.get(image_url)
+        if response.status_code != 200:
+            raise ValueError("Failed to load image from URL")
+        img = Image.open(BytesIO(response.content))
+
+        model = genai.GenerativeModel(model_name)
+
+        if stream:
+            response = model.generate_content([prompt, img], stream=True)
+            textoutput = "\n".join([chunk.text for chunk in response])
+        else:
+            response = model.generate_content([prompt, img])
+            textoutput = response.text
+        
+        return (textoutput,)
+
+
+
 # DisplayText node is forked from AlekPet，thanks to AlekPet！
 class DisplayText_Zho:
     def __init__(self):
@@ -159,11 +276,15 @@ class DisplayText_Zho:
 NODE_CLASS_MAPPINGS = {
     "Gemini_API_Zho": Gemini_API_Zho,
     "Gemini_API_Vsion_ImgURL_Zho": Gemini_API_Vsion_ImgURL_Zho,
+    "Gemini_API_S_Zho": Gemini_API_S_Zho,
+    "Gemini_API_S_Vsion_ImgURL_Zho": Gemini_API_S_Vsion_ImgURL_Zho,
     "DisplayText_Zho": DisplayText_Zho
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Gemini_API_Zho": "✨Gemini_API_Zho",
     "Gemini_API_Vsion_ImgURL_Zho": "✨Gemini_API_Vsion_ImgURL_Zho",
+    "Gemini_API_S_Zho": "㊙️Gemini_Zho",
+    "Gemini_API_S_Vsion_ImgURL_Zho": "㊙️Gemini_Vsion_ImgURL_Zho",
     "DisplayText_Zho": "✨DisplayText_Zho"
 }
