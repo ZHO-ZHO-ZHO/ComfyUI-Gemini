f293ce1912e0387c24a5c22303d81aa87ed72127
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2023-12-20 14:54:12 +0800

    Update GeminiAPINode.py

--- GeminiAPINode.py
+++ GeminiAPINode.py
@@ -1,6 +1,4 @@
 import io
-#from io import BytesIO
-#import torchvision.transforms as transforms
 import google.generativeai as genai
 from PIL import Image
 import torch
@@ -31,18 +29,6 @@ class Gemini_API_Zho:
     FUNCTION = "generate_content"
 
     CATEGORY = "Zho模块组/✨Gemini"
-
-    def tensor_to_image(self, tensor):
-        tensor = tensor.squeeze().mul(255).byte()  # 转换为0-255范围的字节数据
-        tensor = tensor.permute(1, 2, 0)  # 从C x H x W转换为H x W x C
-        image = Image.fromarray(tensor.cpu().numpy(), mode='RGB')
-        return image
-
-    def image_to_byte_stream(self, image):
-        byte_stream = io.BytesIO()
-        image.save(byte_stream, format='JPEG')  # 保存为JPEG格式
-        byte_stream.seek(0)  # 重置流位置
-        return byte_stream
     
     def generate_content(self, prompt, model_name, stream, api_key, image=None):
         if api_key:
@@ -67,14 +53,13 @@ class Gemini_API_Zho:
             else:
                 # 转换图像
                 pil_image = self.tensor_to_image(image)
-                image_stream = self.image_to_byte_stream(pil_image)
 
-                # 使用图像流
+                # 直接使用PIL图像
                 if stream:
-                    response = model.generate_content([prompt, image_stream], stream=True)
+                    response = model.generate_content([prompt, pil_image], stream=True)
                     textoutput = "\n".join([chunk.text for chunk in response])
                 else:
-                    response = model.generate_content([prompt, image_stream])
+                    response = model.generate_content([prompt, pil_image])
                     textoutput = response.text
         
         return (textoutput,)
