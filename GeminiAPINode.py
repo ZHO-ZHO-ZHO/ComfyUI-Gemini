1e66dd2b4f9c8b7601040495ddc1453791523907
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2023-12-20 14:48:52 +0800

    Update GeminiAPINode.py

--- GeminiAPINode.py
+++ GeminiAPINode.py
@@ -1,8 +1,9 @@
 import io
-from io import BytesIO
-import torchvision.transforms as transforms
+#from io import BytesIO
+#import torchvision.transforms as transforms
 import google.generativeai as genai
 from PIL import Image
+import torch
 
 class Gemini_API_Zho:
 
@@ -31,6 +32,18 @@ class Gemini_API_Zho:
 
     CATEGORY = "Zho模块组/✨Gemini"
 
+    def tensor_to_image(self, tensor):
+        tensor = tensor.squeeze().mul(255).byte()  # 转换为0-255范围的字节数据
+        tensor = tensor.permute(1, 2, 0)  # 从C x H x W转换为H x W x C
+        image = Image.fromarray(tensor.cpu().numpy(), mode='RGB')
+        return image
+
+    def image_to_byte_stream(self, image):
+        byte_stream = io.BytesIO()
+        image.save(byte_stream, format='JPEG')  # 保存为JPEG格式
+        byte_stream.seek(0)  # 重置流位置
+        return byte_stream
+    
     def generate_content(self, prompt, model_name, stream, api_key, image=None):
         if api_key:
             self.api_key = api_key
@@ -52,18 +65,16 @@ class Gemini_API_Zho:
             if image == None:
                 raise ValueError("gemini-pro-vision needs image")
             else:
-                #image_pil = transforms.ToPILImage()(image)
-                #buf = io.BytesIO()
-                #image_pil.save(buf, format='PNG')
-                #buf.seek(0)
-                #img = Image.open(buf)
-                img = Image.open(BytesIO(image))
+                # 转换图像
+                pil_image = self.tensor_to_image(image)
+                image_stream = self.image_to_byte_stream(pil_image)
 
+                # 使用图像流
                 if stream:
-                    response = model.generate_content([prompt, img], stream=True)
+                    response = model.generate_content([prompt, image_stream], stream=True)
                     textoutput = "\n".join([chunk.text for chunk in response])
                 else:
-                    response = model.generate_content([prompt, img])
+                    response = model.generate_content([prompt, image_stream])
                     textoutput = response.text
         
         return (textoutput,)
